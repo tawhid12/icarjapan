c764b9ce9c0d413bddd59b82e4e05ac18d5bb622
tawhid12 <tawhid99@gmail.com>
2023-06-12 18:43:13 +0600

    search-complete

--- resources/views/front/welcome.blade.php
+++ resources/views/front/welcome.blade.php
@@ -36,10 +36,6 @@ best car, cheap car,high quality car, motor vehicle,saloon, sedan car, hatchback
 <!-- main section -->
 <main class="">
   <div class="container">
-    <!-- Messenger Chat plugin Code -->
-    <!-- <div id="fb-root"></div> -->
-    <!-- Your Chat plugin code -->
-    <!-- <div id="fb-customer-chat" class="fb-customerchat"></div> -->
 
 <!-- Mainteance Mode -->
 <div class="modal fade" id="myModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -521,29 +517,7 @@ best car, cheap car,high quality car, motor vehicle,saloon, sedan car, hatchback
   @push('scripts')
   
 
-    <!-- <script>
-      var chatbox = document.getElementById('fb-customer-chat');
-      chatbox.setAttribute("page_id", "2464933096867027");
-      chatbox.setAttribute("attribution", "biz_inbox");
-    </script> -->
-
-    <!-- Your SDK code -->
-    <script>
-      /*window.fbAsyncInit = function() {
-        FB.init({
-          xfbml            : true,
-          version          : 'v17.0'
-        });
-      };
-
-      (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));*/
-
+    
       $(document).ready(function() {
         $('#myModal').modal('show');
     });
