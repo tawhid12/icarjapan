3619614f9cc9bf545b089afea9730134625646ac
tawhid12 <tawhid99@gmail.com>
2023-06-13 19:07:09 +0600

    sending mail with test controller

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -7,8 +7,8 @@ use Illuminate\Http\Request;
 class TestController extends Controller
 {
     public function index(){
-        \Mail::send('mail.reply_body', function ($message) {
-            $message->from('tawhid8995@gmail.com', 'ICARJAPAN')
+        \Mail::send('mail.reply_body', [], function ($message) {
+            $message->from('tawhid8995@gmail.com', 'VVMETHOD')
                     ->to('test@icarjapan.com')
                     ->subject('Test');
         });
