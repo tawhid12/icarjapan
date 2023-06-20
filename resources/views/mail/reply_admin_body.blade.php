b37f0a6f71ef852d49b2b9546225fdd56f78a85c
tawhid12 <tawhid99@gmail.com>
2023-06-20 17:31:21 +0600

    correction

--- resources/views/mail/reply_admin_body.blade.php
+++ resources/views/mail/reply_admin_body.blade.php
@@ -23,29 +23,38 @@
         body {
             font-family: "HelveticaNeue-CondensedBold", "HelveticaNeue-Light", "Helvetica Neue Light", "Helvetica Neue", Helvetica, Arial, "Lucida Grande", sans-serif;
         }
-
+        table,tr,th,td{
+            border:1px solid #000;
+        }
     </style>
 </head>
 <body>
-    <h4>Inquiry Received</h4>
-    <div class="wrapper" style="display:flex;">
-        <table style="width:100%;border:1px solid #000;">
+    <h4>Inquiry From User</h4>
+    <div class="wrapper" style="display:flex;justify-content:space-between;">
+        <table style="width:80%;">
             <tr><th colspan="4">Inquiry Details</th</tr>
             <tr>
                 <td><strong>Name:{{$inquiry->name}}</strong></td>
                 <td><strong>Email:{{$inquiry->email}}</strong></td>
             </tr>
             <tr>
-                <td><strong>Email:{{$inquiry->phone}}</strong></td>
+                <td colspan="2"><strong>Phone:{{$inquiry->phone}}</strong></td>
+            </tr>
+            <tr>
+                <td><strong>User Query</strong></td>
                 <td><strong>Email:{{$inquiry->remarks}}</strong></td>
             </tr>
         </table>
-        <table style="width:100%;border:1px solid #000;">
+        <table style="width:80%;border:1px solid #000;">
             <tr><th>Vehicle Details</th</tr>
             <tr>
                 <td><strong>Vehicle Name:{{$v_data->name}}</strong></td>
+            </tr>
+            <tr>
                 <td><strong>Stock ID:{{$v_data->stock_id}}</strong></td>
-                <td><strong>Email:{{$inquiry->chassis_no}}</strong></td>
+            </tr>
+            <tr>
+                <td><strong>Chasis No:{{$inquiry->chassis_no}}</strong></td>
             </tr>
         </table>
     </div>
