afcba09f6ea8ee802ddf9b8b938061140b0995a3
tawhid12 <tawhid99@gmail.com>
2023-08-19 18:48:30 +0600

    correction

--- app/Http/Middleware/CountrySelectionMiddleware.php
+++ app/Http/Middleware/CountrySelectionMiddleware.php
@@ -18,8 +18,8 @@ class CountrySelectionMiddleware
     {
         // Check if the user has selected a country (e.g., using session or user profile)
         if (session()->has('countryName') && session()->has('location')) {
-            return $next($request);
-            //return redirect()->route('front');
+            //return $next($request);
+            return redirect()->route('front');
         }else{
             return redirect()->route('front.countrySelect');
         }
