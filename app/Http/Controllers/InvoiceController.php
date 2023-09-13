729e30117135383df42d2ae65fdf339e29f68983
tawhid12 <tawhid99@gmail.com>
2023-09-13 19:00:18 +0600

    correction

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -103,7 +103,7 @@ class InvoiceController extends Controller
         ->join('fuels', 'fuels.id', '=', 'vehicles.fuel_id')
         ->join('transmissions', 'transmissions.id', '=', 'vehicles.transmission_id')
         ->select('vehicles.*','brands.name as bName','body_types.name as btName','fuels.name as fName','transmissions.name as tName')
-        ->where('vehicles.id', $shipment->vehicle_id)->first();
+        ->where('vehicles.id', $inv->vehicle_id)->first();
         //print_r($v);die;
 
         return view('sales_module.invoice.proforma_client_invoice', compact('v','shipment','account_info', 'inv', 'com_info', 'client_data', 'client_details'));
