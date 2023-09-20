dc685d6f329ae68683471fbe0cd778f85bb83cce
tawhid12 <tawhid99@gmail.com>
2023-09-20 18:58:10 +0600

    correction

--- resources/views/front/partial/brand-side-bar.blade.php
+++ resources/views/front/partial/brand-side-bar.blade.php
@@ -8,7 +8,7 @@
             <p class="card-text">
                 <a href="{{route('brand',strtolower($b->slug_name))}}" style="text-decoration:none;color:#000;">
                 @if(!empty($b->image))
-                
+                <img src="{{route('resizeImage',['brands',$b->image,18,26])}}" alt="" />
                 @endif
                  {{$b->name}} ({{$b->vehicles_count}})</a>
             </p>
