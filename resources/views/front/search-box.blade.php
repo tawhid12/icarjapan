ec6f7bc9932abc37a437a772dbd258bee6e34e7e
tawhid12 <tawhid99@gmail.com>
2023-09-21 16:58:04 +0600

    correction

--- resources/views/front/search-box.blade.php
+++ resources/views/front/search-box.blade.php
@@ -2,7 +2,7 @@
 @csrf()
 <!-- mid row 1 -->
 <div class="mid-row-1">
-    <div class="input-group d-flex align-items-center mb-2">
+    <div class="input-group d-flex align-items-center my-2">
         <span class="input-group-text">Search Car</span>
         <input name="sdata" type="text" id="item_search" class="form-control ui-autocomplete-input"/>
         <button type="submit" class="input-group-text"><i class="bi bi-search"></i></button>
