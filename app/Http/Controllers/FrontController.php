574369e3f9bc90dead6e137764130ddd4ee5a89d
tawhid12 <tawhid99@gmail.com>
2023-10-07 14:51:16 +0600

    correction

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -125,7 +125,8 @@ class FrontController extends Controller
             }*/
         //}
 
-       
+        echo '<pre>';
+    print_r(session());die;
         $current_locale_data = Carbon::now($location['geoplugin_timezone']);
         /*==New Arival== | New Affordable==*/
         $new_arivals = DB::table('vehicles')
