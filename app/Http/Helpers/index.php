c3cd127273b11dc5d81db83525a4670953a2de5b
tawhid12 <tawhid99@gmail.com>
2023-10-31 11:29:43 +0600

    correction

--- app/Http/Helpers/index.php
+++ app/Http/Helpers/index.php
@@ -16,7 +16,7 @@ function countryIp(){
             session()->put('countryName', $countryName);
             session()->put('location', $location);
             session()->put('current_locale_data', $current_locale_data);
-            
+            return redirect()->back();
         }else{
             unset($_SESSION['countryName']);
             unset($_SESSION['location']);
