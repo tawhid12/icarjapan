00e14fabad05968d85a9f77bb0a2e27e69111466
tawhid12 <tawhid99@gmail.com>
2023-10-31 16:59:59 +0600

    correction

--- app/Http/Controllers/InquiryController.php
+++ app/Http/Controllers/InquiryController.php
@@ -6,6 +6,7 @@ use App\Models\Inquiry;
 use App\Models\Vehicle\Vehicle;
 use Illuminate\Http\Request;
 use Toastr;
+
 class InquiryController extends Controller
 {
     /**
@@ -15,14 +16,19 @@ class InquiryController extends Controller
      */
     public function index()
     {
-        if(currentUser() == 'user'){
-            $all_in = Inquiry::where('created_by',currentUserId())->get();
-            return view('user.inquiry.index',compact('all_in'));
-        }else{
+        if (currentUser() == 'user') {
+            $all_in = Inquiry::where('created_by', currentUserId())->get();
+            $con_detl = ConsigneeDetail::paginate(10);
+            countryIp();
+            $location =  request()->session()->get('location');
+            $countryName =  request()->session()->get('countryName');
+            if (isset($location['geoplugin_currencyCode']) && isset($location['geoplugin_currencyConverter']) && isset($countryName->id)) {
+                return view('user.inquiry.index', compact('all_in','location'));
+            }
+        } else {
             $all_in = Inquiry::all();
-            return view('settings.inquiry.index',compact('all_in'));
+            return view('settings.inquiry.index', compact('all_in'));
         }
-        
     }
 
     /**
@@ -43,41 +49,40 @@ class InquiryController extends Controller
      */
     public function store(Request $request)
     {
-        try{
-            $in=new Inquiry;
-            $in->name=$request->name;
-            $in->country_id=$request->country_id;
-            $in->city=$request->city;
-            $in->email=$request->email;
-            $in->phone=$request->phone;
-            $in->remarks=$request->remarks;
-            $in->vehicle_id=$request->vehicle_id;
-            $in->created_by=currentUserId();
-            
-            if($in->save()){
+        try {
+            $in = new Inquiry;
+            $in->name = $request->name;
+            $in->country_id = $request->country_id;
+            $in->city = $request->city;
+            $in->email = $request->email;
+            $in->phone = $request->phone;
+            $in->remarks = $request->remarks;
+            $in->vehicle_id = $request->vehicle_id;
+            $in->created_by = currentUserId();
+
+            if ($in->save()) {
                 $inquiry = Inquiry::findOrFail($in->id);
-                $v_data = Vehicle::where('id',$inquiry->vehicle_id)->first();
-                $type = 1;//Inquiry Received
+                $v_data = Vehicle::where('id', $inquiry->vehicle_id)->first();
+                $type = 1; //Inquiry Received
                 /*To User */
-                \Mail::send('mail.reply_user_body', ['inquiry' => $inquiry,'type' => $type], function ($message) use ($inquiry,$v_data){
+                \Mail::send('mail.reply_user_body', ['inquiry' => $inquiry, 'type' => $type], function ($message) use ($inquiry, $v_data) {
                     $message->from('info@icarjapan.com', 'Icarjapan')
-                            ->to($inquiry->email)
-                            ->subject('Inquiry For '.$v_data->name.' and Stock Id '.$v_data->stock_id);
+                        ->to($inquiry->email)
+                        ->subject('Inquiry For ' . $v_data->name . ' and Stock Id ' . $v_data->stock_id);
                 });
                 /*To Admin */
-                \Mail::send('mail.reply_admin_body', ['inquiry' => $inquiry,'v_data' => $v_data,'type' => $type], function ($message) use ($v_data){
+                \Mail::send('mail.reply_admin_body', ['inquiry' => $inquiry, 'v_data' => $v_data, 'type' => $type], function ($message) use ($v_data) {
                     $message->from('info@icarjapan.com', 'Icarjapan')
-                            ->to('icarjapanofficial@gmail.com')
-                            ->subject('Inquiry For '.$v_data->name.' and Stock Id '.$v_data->stock_id);
+                        ->to('icarjapanofficial@gmail.com')
+                        ->subject('Inquiry For ' . $v_data->name . ' and Stock Id ' . $v_data->stock_id);
                 });
                 // Redirect user to intended URL
                 return redirect()->back()->with(Toastr::success('Inquiry Received!', 'Success', ["positionClass" => "toast-top-right"]));;
                 //return redirect()->route(currentUser().'.brand.index')
-            }
-            else{
+            } else {
                 return redirect()->back()->withInput()->with(Toastr::error('Please try again!', 'Fail', ["positionClass" => "toast-top-right"]));
             }
-        }catch(Exception $e){
+        } catch (Exception $e) {
             //dd($e);
             return redirect()->back()->withInput()->with(Toastr::error('Please try again!', 'Fail', ["positionClass" => "toast-top-right"]));
         }
@@ -103,7 +108,7 @@ class InquiryController extends Controller
     public function edit($id)
     {
         $inquiry = Inquiry::findOrFail(encryptor('decrypt', $id));
-        return view('settings.inquiry.edit',compact('inquiry'));
+        return view('settings.inquiry.edit', compact('inquiry'));
     }
 
     /**
@@ -119,21 +124,21 @@ class InquiryController extends Controller
             $inquiry = Inquiry::findOrFail(encryptor('decrypt', $id));
             $inquiry->reply = $request->reply;
             $inquiry->replied_by = currentUserId();
-            if($inquiry->save()){
+            if ($inquiry->save()) {
                 $inquiry = Inquiry::findOrFail($inquiry->id);
-                $v_data = Vehicle::where('id',$inquiry->vehicle_id)->first();
+                $v_data = Vehicle::where('id', $inquiry->vehicle_id)->first();
                 /*To User */
-                $type = 2;//reply
-                \Mail::send('mail.reply_user_body', ['inquiry' => $inquiry,'type' => $type], function ($message) use ($inquiry,$v_data){
+                $type = 2; //reply
+                \Mail::send('mail.reply_user_body', ['inquiry' => $inquiry, 'type' => $type], function ($message) use ($inquiry, $v_data) {
                     $message->from('info@icarjapan.com', 'Icarjapan')
-                            ->to($inquiry->email)
-                            ->subject('Reply For '.$v_data->name.' and Stock Id '.$v_data->stock_id);
+                        ->to($inquiry->email)
+                        ->subject('Reply For ' . $v_data->name . ' and Stock Id ' . $v_data->stock_id);
                 });
                 /*To Admin */
-                \Mail::send('mail.reply_admin_body', ['inquiry' => $inquiry,'v_data' => $v_data,'type' => $type], function ($message) use ($v_data){
+                \Mail::send('mail.reply_admin_body', ['inquiry' => $inquiry, 'v_data' => $v_data, 'type' => $type], function ($message) use ($v_data) {
                     $message->from('info@icarjapan.com', 'Icarjapan')
-                            ->to('icarjapanofficial@gmail.com')
-                            ->subject('Reply For '.$v_data->name.' and Stock Id '.$v_data->stock_id);
+                        ->to('icarjapanofficial@gmail.com')
+                        ->subject('Reply For ' . $v_data->name . ' and Stock Id ' . $v_data->stock_id);
                 });
             }
             if ($inquiry->save()) {
