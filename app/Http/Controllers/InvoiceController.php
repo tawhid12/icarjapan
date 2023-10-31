e9f487f98a65ac8bd5cf39512ea9079317501671
tawhid12 <tawhid99@gmail.com>
2023-10-31 17:23:02 +0600

    correction

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -31,12 +31,11 @@ class InvoiceController extends Controller
             $invoices = Invoice::where('executive_id', currentUserId())->paginate(10);
             return view('sales.invoice.index', compact('invoices'));
         } else if (currentUser() == 'user') {
-            $invoices = Invoice::where('customer_id', currentUserId())->paginate(10);
+            $invoices = Invoice::where('client_id', currentUserId())->paginate(10);
             countryIp();
             $location =  request()->session()->get('location');
             $countryName =  request()->session()->get('countryName');
             if (isset($location['geoplugin_currencyCode']) && isset($location['geoplugin_currencyConverter']) && isset($countryName->id)) {
-                $invoices = Invoice::where('executive_id', currentUserId())->paginate(10);
                 return view('user.invoice.index', compact('invoices', 'location'));
             }
         } else {
