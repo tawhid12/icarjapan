c883133bbc33c8ce57389637f5b2ad58ba3348ef
tawhid12 <tawhid99@gmail.com>
2023-10-31 16:36:30 +0600

    correction

--- app/Http/Controllers/Settings/UserProfileController.php
+++ app/Http/Controllers/Settings/UserProfileController.php
@@ -32,7 +32,12 @@ class UserProfileController extends Controller
         if(currentUser() == 'superadmin' || currentUser() == 'salesexecutive'){
             return view('settings.general.profile',compact('user','countries','ports','com_info'));
         }else{
-            return view('settings.user.profile',compact('user','countries','ports','com_info'));
+            countryIp();
+            $location =  request()->session()->get('location');
+            $countryName =  request()->session()->get('countryName');
+            if (isset($location['geoplugin_currencyCode']) && isset($location['geoplugin_currencyConverter']) && isset($countryName->id)) {
+                return view('settings.user.profile',compact('user','countries','ports','com_info'));
+            }
         }
         
     }
