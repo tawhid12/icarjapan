0fe2833f9e1310f56a07d263e69e3c62c23fed12
tawhid12 <tawhid99@gmail.com>
2023-10-31 11:11:55 +0600

    correction

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -290,7 +290,7 @@ die;*/
                 ->facebook()
                 ->twitter()
                 ->whatsapp();
-            return view('front.single', compact('current_locale_data','countries', 'v_images', 'v', 'brand', 'sub_brand_id', 'shareComponent', 'url', 'cover_img', 'recomended'));
+            return view('front.single', compact('countries', 'v_images', 'v', 'brand', 'sub_brand_id', 'shareComponent', 'url', 'cover_img', 'recomended'));
         }else{
             countryIp();
         }
