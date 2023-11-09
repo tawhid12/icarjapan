171eff175e9906087b95bf83d30d5bcd29cecbc8
tawhid12 <tawhid99@gmail.com>
2023-11-09 13:31:54 +0600

    correction

--- resources/views/sales_module/search_vehicle.blade.php
+++ resources/views/sales_module/search_vehicle.blade.php
@@ -619,7 +619,8 @@
                             @endif
                             <p class="stock-text m-0">Stock ID : {{$v->stock_id}}</p>
                             @if($v->r_status != null)
-                            <p class="m-0 text-danger"><strong>Reserved For | CM ID:-{{\DB::table('reserved_vehicles')->where('vehicle_id',$v->id)->first()->user_id}}</strong></p>
+                            <p class="m-0 text-danger"><strong>Reserved </strong></p>
+                            {{--For | CM ID:-\DB::table('reserved_vehicles')->where('vehicle_id',$v->id)->first()->user_id--}}
                             @endif
                             @if($v->sold_status)
                             <p class="m-0 text-success"><strong>SOLD</strong></p>
