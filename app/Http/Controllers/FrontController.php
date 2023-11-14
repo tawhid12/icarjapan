93c3ac7c746b3a68b61a9758c94658181e95c2d9
tawhid12 <tawhid99@gmail.com>
2023-11-14 17:53:37 +0600

    correction

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -29,25 +29,15 @@ class FrontController extends Controller
 {
     public function countrySelectpost(Request $request)
     {
-
         $countryName = Country::where('code', $request->code)->first();
-        if ($countryName) {
+        $countryName = Country::where('code', $request->code)->first();
+        $location = unserialize(file_get_contents('http://www.geoplugin.net/php.gp?ip=' . $countryName->ip_address));
+        Log::info($location);
+        Log::info($countryName);
+        /*if ($countryName->ip_address) {
             $location = unserialize(file_get_contents('http://www.geoplugin.net/php.gp?ip=' . $countryName->ip_address));
-            if(isset($location['geoplugin_status']) and $location['geoplugin_status'] == 200){
-                $location = unserialize(file_get_contents('http://www.geoplugin.net/php.gp?ip=' . $countryName->ip_address));
-                $current_locale_data = Carbon::now($location['geoplugin_timezone']);
-                $countryName = Country::where('code', $location['geoplugin_countryCode'])->first();
-                session()->put('countryName', $countryName);
-                session()->put('location', $location);
-                session()->put('current_locale_data', $current_locale_data);
-                return redirect()->route('front');
-            }else{
-                countryIp(); 
-            }
-            
-        }else{
-            countryIp(); 
-        }
+            return redirect()->route('front');
+        }*/
     }
     public function countrySelect()
     {
