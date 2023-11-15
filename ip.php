7c0ddefc42afa2d6c96daf5144da9bf2cf1c8c3d
tawhid12 <tawhid99@gmail.com>
2023-11-15 09:16:40 +0600

    correction

--- ip.php
+++ ip.php
@@ -1,3 +1,9 @@
 <?php
-	echo file_get_contents("https://www.geoplugin.com/ip.php");
+$arrContextOptions=array(
+    "ssl"=>array(
+        "verify_peer"=>false,
+        "verify_peer_name"=>false,
+    ),
+); 
+	echo file_get_contents("https://www.geoplugin.com/ip.php", false, stream_context_create($arrContextOptions));
 ?>
