a349a83f087f7773ec0019a4b30c843f177aa11b
tawhid12 <tawhid99@gmail.com>
2023-12-28 11:51:39 +0600

    correction

--- ip.php
+++ ip.php
@@ -1,6 +1,6 @@
 <?php
 $user_ip = getenv('REMOTE_ADDR');
-$location = json_decode(file_get_contents("https://extreme-ip-lookup.com/json/$user_ip?key=9x9yyW5zMrdFwAKLH5jO"));
+$location =  unserialize(json_decode(file_get_contents("https://extreme-ip-lookup.com/json/$user_ip?key=9x9yyW5zMrdFwAKLH5jO")));
 echo '<pre>';
 print_r($location);
 ?>
