8c36500d379a5f77120bfb8782034f56e5ee341a
tawhid12 <tawhid99@gmail.com>
2023-12-28 12:40:50 +0600

    correction

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -79,7 +79,7 @@ class FrontController extends Controller
         die;*/
         if (isset($currency['geoplugin_currencyCode']) && isset($currency['geoplugin_currencyConverter']) && isset($countryName->id)) {
           
-            $current_locale_data = Carbon::now($location['geoplugin_timezone']);
+            $current_locale_data = Carbon::now($location->timezone);
             /*==New Arival== | New Affordable==*/
             $new_arivals = DB::table('vehicles')
                 ->select('vehicles.id as vid', 'vehicles.r_status', 'vehicles.name', 'vehicles.price', 'vehicles.discount', 'vehicles.manu_year', 'vehicles.chassis_no', 'vehicles.stock_id', 'brands.slug_name as b_slug', 'sub_brands.slug_name as sb_slug')
