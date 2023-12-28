9e1c16f806daefd496d68a8150802c64e9ffc284
tawhid12 <tawhid99@gmail.com>
2023-12-28 14:38:15 +0600

    correction

--- app/Http/Controllers/InquiryController.php
+++ app/Http/Controllers/InquiryController.php
@@ -19,11 +19,12 @@ class InquiryController extends Controller
         if (currentUser() == 'user') {
             $all_in = Inquiry::where('created_by', currentUserId())->get();
 
-            countryIp();
             $location =  request()->session()->get('location');
             $countryName =  request()->session()->get('countryName');
             if (isset($location['geoplugin_currencyCode']) && isset($location['geoplugin_currencyConverter']) && isset($countryName->id)) {
                 return view('user.inquiry.index', compact('all_in','location'));
+            }else{
+                countryIp();
             }
         } else {
             $all_in = Inquiry::all();
