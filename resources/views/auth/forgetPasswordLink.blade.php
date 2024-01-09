71eb80816eec9ea9da21c13539e0e61b45b763f6
tawhid12 <tawhid99@gmail.com>
2024-01-09 15:25:59 +0600

    correction

--- resources/views/auth/forgetPasswordLink.blade.php
+++ resources/views/auth/forgetPasswordLink.blade.php
@@ -9,14 +9,10 @@
                   <div class="card-header">Reset Password</div>
                   <div class="card-body">
   
-                    @if (Session::has('message'))
-                         <div class="alert alert-success" role="alert">
-                            {{ Session::get('message') }}
-                        </div>
-                    @endif
-  
-                      <form action="{{ route('forget.password.post') }}" method="POST">
+                      <form action="{{ route('reset.password.post') }}" method="POST">
                           @csrf
+                          <input type="hidden" name="token" value="{{ $token }}">
+  
                           <div class="form-group row">
                               <label for="email_address" class="col-md-4 col-form-label text-md-right">E-Mail Address</label>
                               <div class="col-md-6">
@@ -26,9 +22,30 @@
                                   @endif
                               </div>
                           </div>
+  
+                          <div class="form-group row">
+                              <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
+                              <div class="col-md-6">
+                                  <input type="password" id="password" class="form-control" name="password" required autofocus>
+                                  @if ($errors->has('password'))
+                                      <span class="text-danger">{{ $errors->first('password') }}</span>
+                                  @endif
+                              </div>
+                          </div>
+  
+                          <div class="form-group row">
+                              <label for="password-confirm" class="col-md-4 col-form-label text-md-right">Confirm Password</label>
+                              <div class="col-md-6">
+                                  <input type="password" id="password-confirm" class="form-control" name="password_confirmation" required autofocus>
+                                  @if ($errors->has('password_confirmation'))
+                                      <span class="text-danger">{{ $errors->first('password_confirmation') }}</span>
+                                  @endif
+                              </div>
+                          </div>
+  
                           <div class="col-md-6 offset-md-4">
                               <button type="submit" class="btn btn-primary">
-                                  Send Password Reset Link
+                                  Reset Password
                               </button>
                           </div>
                       </form>
