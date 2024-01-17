a3decac2529b30c7475d732e78dd465a67e15539
tawhid12 <tawhid99@gmail.com>
2024-01-17 12:08:35 +0600

    correction

--- app/Http/Controllers/ShipmentDetailController.php
+++ app/Http/Controllers/ShipmentDetailController.php
@@ -109,7 +109,7 @@ class ShipmentDetailController extends Controller
      */
     public function edit($id)
     {
-        $shipment = ShipmentDetail::where('reserve_id',encryptor('decrypt', $id));
+        $shipment = ShipmentDetail::where('reserve_id',encryptor('decrypt', $id))->first();
         $user = User::where('id', $shipment->client_id)->first();
         $countries = Country::all();
         $ports = Port::all();
