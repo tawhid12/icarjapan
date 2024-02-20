5295268af22bd6d10c6c10fd6b3c2eb537c6e486
tawhid12 <tawhid99@gmail.com>
2024-02-20 17:11:13 +0600

    correction

--- resources/views/review/index.blade.php
+++ resources/views/review/index.blade.php
@@ -3,5 +3,15 @@
 @section('pageTitle', 'Review List')
 @section('pageSubTitle', 'List')
 @section('content')
-
+<section class="section">
+    <div class="card">
+        @include('layout.message')
+        <div class="row" id="table-bordered">
+            <div class="col-md-12">
+                <!-- table bordered -->
+               
+            </div>
+        </div>
+    </div>
+</section>
 @endsection
