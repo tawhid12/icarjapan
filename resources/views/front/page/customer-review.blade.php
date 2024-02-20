f2911d5e3391a34c5f3df0be638dfebda8563735
tawhid12 <tawhid99@gmail.com>
2024-02-20 16:03:08 +0600

    correction

--- resources/views/front/page/customer-review.blade.php
+++ resources/views/front/page/customer-review.blade.php
@@ -29,45 +29,46 @@
                     <div class="review-user-body my-3">
                         <div class="row my-1 border-bottom">
                             @forelse ($reviews as $review)
-                              <div class="col-sm-4 review-user-p-img">
-                                <div class="slider">
-                                @forelse ($review->review_images as $rimg)
-                                <img class="img-fluid" src="{{asset('uploads/review/'.$rimg->upload)}}" alt="" />
+                                <div class="col-sm-4 review-user-p-img">
+                                    <div class="slider">
+                                        @forelse ($review->review_images as $rimg)
+                                            <img class="img-fluid" src="{{ asset('uploads/review/' . $rimg->upload) }}"
+                                                alt="" />
+                                    </div>
                                 @empty
-                                <img class="img-fluid" src="https://ui-avatars.com/api/?name={{$review->user?->name}}" alt="" />
-                                @endforelse
-                              </div>
-                              </div>
-                              <div class="col-sm-8 review-user">
-                                <div class="d-flex">
-                                  @if($review->cimage)
-                                  <img class="img-fluid" src="{{asset('uploads/reviews/'.$review->cimage)}}" alt="" />
-                                  @endif
-                                  <div>
-                                    <p>{{$review->user->name}}
-                                      @if($review->rating > 0)
-                                      <span class="review">
-                                        @php 
-                                        for($i=1; $i<=$review->rating; $i++){
-                                          echo '<i class="bi bi-star" style=""></i>';
-                                        }
-                                        @endphp
-                                      </span>
-                                      @endif
-                                    </p>
-                                    <p>{{ \Carbon\Carbon::parse($review->created_at)->format('F j, Y') }}</p>
-                                  </div>
+                                    <img class="img-fluid" src="https://ui-avatars.com/api/?name={{ $review->user?->name }}"
+                                        alt="" />
                                 </div>
-                                <p>{{$review->comment}}</p>
-                              </div>
-                            
-                            @empty
                             @endforelse
-                          </div>
-                    
+                            <div class="col-sm-8 review-user">
+                                <div class="d-flex">
+                                    @if ($review->cimage)
+                                        <img class="img-fluid" src="{{ asset('uploads/reviews/' . $review->cimage) }}"
+                                            alt="" />
+                                    @endif
+                                    <div>
+                                        <p>{{ $review->user->name }}
+                                            @if ($review->rating > 0)
+                                                <span class="review">
+                                                    @php
+                                                        for ($i = 1; $i <= $review->rating; $i++) {
+                                                            echo '<i class="bi bi-star" style=""></i>';
+                                                        }
+                                                    @endphp
+                                                </span>
+                                            @endif
+                                        </p>
+                                        <p>{{ \Carbon\Carbon::parse($review->created_at)->format('F j, Y') }}</p>
+                                    </div>
+                                </div>
+                                <p>{{ $review->comment }}</p>
+                            </div>
+                        @empty
+                            @endforelse
+                        </div>
+                    </div>
+                    {{ $reviews->links() }}
                 </div>
-                {{ $reviews->links() }}
             </div>
         </div>
-    </div>
-@endsection
+    @endsection
