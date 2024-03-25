b3d97dfc659b3a4d826f08cb5b41ecfb860ed006
tawhid12 <tawhid99@gmail.com>
2024-03-25 13:27:08 +0600

    Correction

--- resources/views/front/page/page.blade.php
+++ resources/views/front/page/page.blade.php
@@ -10,7 +10,7 @@
 <div class="container my-4">
     <div class="row">
         <div class="col-md-12">
-            <h4 class="text-center" style="color:#ED2129">{{$page->title}}</h4>
+            <h3 class="text-center" style="color:#ED2129">{{$page->title}}</h3>
             {!!$page->details!!}
         </div>
     </div>
